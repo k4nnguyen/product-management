fbdd5cd61fbfacca6b0d72a460050323530d757e
Anng <grilledog296@gmail.com>
2025-08-05 13:46:06 +0700

    CSS For Admin Page

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const routesAdmin = require("./routes/admin/index.route");
 const port = process.env.PORT;
 const app = express();
 const database = require("./config/database");
-
+const systemConfig = require("./config/system");
 // Set up folder, view engine and database
 app.use(express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "pug");
 database.connect();
 
+// Local Variable to Pug
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 // Routes
 routes(app);
 routesAdmin(app);
